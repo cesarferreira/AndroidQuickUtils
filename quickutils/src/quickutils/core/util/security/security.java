89adfa48aa47210378eb7b4ca94f7d0b645e0298
cesar ferreira <cesar.manuel.ferreira@gmail.com>
2013-04-30 19:10:21 +0100

    Updated Javadocs
    Added the following methods:
    public static String encodeBase64(String toEncode, int flags)
    public static String encodeBase64(String toEncode)
    public static String decodeBase64(String toDecode)
    public static String decodeBase64(String toDecode, int flags)

--- quickutils/src/quickutils/core/util/security/security.java
+++ quickutils/src/quickutils/core/util/security/security.java
@@ -5,14 +5,113 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import quickutils.core.QuickUtils;
+import android.util.Base64;
 
-public  class security {
+public class security {
 
 	/**
 	 * private constructor
 	 */
-//	private security() {
-//	}
+	// private security() {
+	// }
+
+	/**
+	 * Encode in base64
+	 * 
+	 * @param toEncode
+	 *            String to be encoded
+	 * @param flags
+	 *            flags to encode the String
+	 * @return encoded String in base64
+	 */
+	public static String encodeBase64(String toEncode, int flags) {
+		return privateBase64Encoder(toEncode, flags);
+
+	}
+
+	/**
+	 * Encode in base64
+	 * 
+	 * @param toEncode
+	 *            String to be encoded
+	 * @return encoded String in base64
+	 */
+	public static String encodeBase64(String toEncode) {
+		return privateBase64Encoder(toEncode, -1);
+	}
+
+	/**
+	 * private Encoder in base64
+	 * 
+	 * @param toEncode
+	 *            String to be encoded
+	 * @param flags
+	 *            flags to encode the String
+	 * @return encoded String in base64
+	 */
+	private static String privateBase64Encoder(String toEncode, int flags) {
+		byte[] data = null;
+		try {
+			data = toEncode.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+
+		if (flags == -1) {
+			flags = Base64.DEFAULT;
+		}
+
+		return Base64.encodeToString(data, flags);
+	}
+
+	/**
+	 * Decode in base64
+	 * 
+	 * @param toDecode
+	 *            String to be encoded
+	 * @return decoded String in base64
+	 */
+	public static String decodeBase64(String toDecode) {
+		return privateBase64Decoder(toDecode, -1);
+	}
+
+	/**
+	 * Decode in base64
+	 * 
+	 * @param toDecode
+	 *            String to be encoded
+	 * @param flags
+	 *            flags to decode the String
+	 * @return decoded String in base64
+	 */
+	public static String decodeBase64(String toDecode, int flags) {
+		return privateBase64Decoder(toDecode, flags);
+	}
+
+	/**
+	 * Private decoder in base64
+	 * 
+	 * @param toDecode
+	 *            String to be encoded
+	 * @param flags
+	 *            flags to decode the String
+	 * @return decoded String in base64
+	 */
+	private static String privateBase64Decoder(String decode, int flags) {
+		if (flags == -1) {
+			flags = Base64.DEFAULT;
+		}
+
+		byte[] data1 = Base64.decode(decode, flags);
+		String decodedBase64 = null;
+		try {
+			decodedBase64 = new String(data1, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return decodedBase64;
+	}
 
 	/**
 	 * Calculate the MD5 of a given String
@@ -25,8 +124,7 @@ public  class security {
 		byte[] hash;
 
 		try {
-			hash = MessageDigest.getInstance("MD5").digest(
-					string.getBytes("UTF-8"));
+			hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException("Huh, MD5 should be supported?", e);
 		} catch (UnsupportedEncodingException e) {
@@ -75,8 +173,7 @@ public  class security {
 			int halfbyte = (b >>> 4) & 0x0F;
 			int two_halfs = 0;
 			do {
-				buf.append((0 <= halfbyte) && (halfbyte <= 9) ? (char) ('0' + halfbyte)
-						: (char) ('a' + (halfbyte - 10)));
+				buf.append((0 <= halfbyte) && (halfbyte <= 9) ? (char) ('0' + halfbyte) : (char) ('a' + (halfbyte - 10)));
 				halfbyte = b & 0x0F;
 			} while (two_halfs++ < 1);
 		}
