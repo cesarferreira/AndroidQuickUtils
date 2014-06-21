b06393b5c22464036e77ed51e590de591befb8b3
César Ferreira <cesar.manuel.ferreira@gmail.com>
2014-06-21 18:34:33 +0100

    Added circleImageView

--- demo/src/main/java/com/cesarferreira/quickutils/demo/TestingBlur.java
+++ demo/src/main/java/com/cesarferreira/quickutils/demo/TestingBlur.java
@@ -12,12 +12,14 @@ import quickutils.core.interfaces.OnEventListener;
 public class TestingBlur extends Activity {
 
     private ImageView imageView;
+    private ImageView smalliImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing_blur);
         imageView = (ImageView) findViewById(R.id.imageView);
+        smalliImageView = (ImageView) findViewById(R.id.smallImageView);
     }
 
 
